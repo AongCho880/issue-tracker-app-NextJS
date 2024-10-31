9d659480e85e9de0f87b33ce1bf09393aab23170
AongCho880 <aongcho880@gmail.com>
2024-10-31 08:44:21 +0600

    Customize Radix UI Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import "./theme-config.css";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
 import { NavBar } from "./NavBar";
+
+const inter = Inter({
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
+});
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,11 +35,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Theme>
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Theme accentColor="violet" radius="small" scaling="110%">
           <NavBar />
           <main className='p-5'>
             {children}
