82bac159af371f0086cb5b14b1ce4b2ff3193c40
AongCho880 <aongcho880@gmail.com>
2024-11-28 00:30:15 +0600

    Update

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,19 +1,13 @@
-import { Box, Card, Flex } from '@radix-ui/themes'
-import React from 'react'
+import { Box } from '@radix-ui/themes'
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
+
 const LoadingNewIssuePage = () => {
   return (
-    <Box className='max-w-xl'>
-      <Skeleton />
-      <Flex className='space-x-3' my='2'>
-        <Skeleton width='5rem'/>
-        <Skeleton width='8rem'/>
-      </Flex>
-      <Card className='prose' mt='4'>
-        <Skeleton count={3} />
-      </Card>
+    <Box className='max-w-xl space-y-3'>
+      <Skeleton height='3rem' />
+      <Skeleton height='20rem'/>
     </Box>
   )
 }
