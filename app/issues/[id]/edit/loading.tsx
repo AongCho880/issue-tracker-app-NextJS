c4a7e8a4480bb92b440737d032d6aef60853ffff
AongCho880 <aongcho880@gmail.com>
2024-12-03 10:37:25 +0600

    Build the edit issue page

---
+++ app/issues/[id]/edit/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loadingEditIssuePage = () => {
+  return (
+    <div>loading...</div>
+  )
+}
+
+export default loadingEditIssuePage
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,8 +1,8 @@
 import prisma from '@/prisma/client';
 import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import EditIssueButton from './edit/EditIssueButton';
-import IssueDetails from './edit/IssueDetails';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 // import delay from 'delay';
 
 interface Props {
