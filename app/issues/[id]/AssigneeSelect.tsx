210de5949670756af52ec8b5cc728941791a2fdf
AongCho880 <aongcho880@gmail.com>
2025-02-17 13:21:13 +0600

    Assign a issue to a user

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,39 +1,46 @@
 'use client';
-import { User } from '@prisma/client';
-import { Select } from '@radix-ui/themes'
+import { Issue, User } from '@prisma/client';
+import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React, { use, useEffect, useState } from 'react'
+import React from 'react';
 import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css'
+import 'react-loading-skeleton/dist/skeleton.css';
 
-
-
-const AssigneeSelect = () => {
-
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const { data: users, error, isLoading } = useQuery<User[]>({
     queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then(res => res.data),
-    staleTime: 60 * 1000, // 60 sec
-    retry: 3
+    queryFn: () => axios.get('/api/users').then((res) => res.data),
+    staleTime: 60 * 1000,
+    retry: 3,
   });
 
-  if(isLoading) return <Skeleton />
-  if(error) return null;
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
 
   return (
-    <Select.Root>
-      <Select.Trigger placeholder='Assign...' />
+    <Select.Root
+      defaultValue={issue.assignedToUserId || 'null'}
+      onValueChange={(userId) => {
+        axios.patch('/api/issues/' + issue.id, {
+          assignedToUserId: userId === 'null' ? null : userId,
+        });
+      }}
+    >
+      <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users?.map(user => (
-            <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+          <Select.Item value="null">Unassigned</Select.Item>
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
           ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default AssigneeSelect
+export default AssigneeSelect;
